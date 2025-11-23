bab6b8f5f64d31e8c006ac6a4710b61921619a6e
ClarensAugustin <161627356+ClarensAugustin@users.noreply.github.com>
2025-11-23 09:42:01 -0500

    Update Divide.java

--- Divide.java
+++ Divide.java
@@ -2,6 +2,10 @@
 public class Divide extends Binop {
 
     public double eval(double left, double right) {
+        // if divide by zero
+        if (right == 0.0 || Math.abs(right) < 1e-10) {
+            return 1.0;
+        }
         return left / right;
     }
 
@@ -11,4 +15,5 @@ public class Divide extends Binop {
     }
 
 }
+
 
